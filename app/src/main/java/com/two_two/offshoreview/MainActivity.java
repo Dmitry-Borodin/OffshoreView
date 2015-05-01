88ea2325d6035687c53ed08b5e053cc62db90f93
Dmitry Borodin <mailborodin@gmail.com>
2015-05-01 14:12:14 +0300

    some text before merge

--- app/src/main/java/com/two_two/offshoreview/MainActivity.java
+++ app/src/main/java/com/two_two/offshoreview/MainActivity.java
@@ -1,7 +1,7 @@
 package com.two_two.offshoreview;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
